7d3d0e48b5d63de282776d5e8e5ed8a7213cff57
Kyosuke Morita <kyosuke1029@icloud.com>
2024-03-07 09:22:01 +0000

    add requirements..txt

--- store_docs.py
+++ store_docs.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain.document_loaders import CSVLoader
 from langchain.vectorstores import Chroma
 
-logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s')
+logging.basicConfig(format="%(process)d-%(levelname)s-%(message)s")
 
 
 logging.info("Loading CSV file")
@@ -20,5 +20,7 @@ docs = text_splitter.split_documents(documents)
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
 logging.info("Storing document in the vector storage")
-db = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db_sentence_trm")
+db = Chroma.from_documents(
+    docs, embedding_function, persist_directory="./chroma_db_sentence_trm"
+)
 logging.info("Job complete")

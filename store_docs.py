779c42fc526bffc6e2172dbc5cf427db15e2b9ea
Kyosuke Morita <kyosuke1029@icloud.com>
2024-03-07 00:18:46 +0000

    use sentence trms

--- store_docs.py
+++ store_docs.py
@@ -1,4 +1,6 @@
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.document_loaders import CSVLoader
 from langchain.vectorstores import Chroma
@@ -7,9 +9,9 @@ from langchain.vectorstores import Chroma
 loader = CSVLoader("decisions_2023.csv", encoding="utf-8")
 documents = loader.load()
 
-text_splitter = CharacterTextSplitter(chunk_size=512, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=512, chunk_overlap=64)
 docs = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings()
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-db2 = Chroma.from_documents(docs, embeddings, persist_directory="./chroma_db")
+db = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db_sentence_trm")

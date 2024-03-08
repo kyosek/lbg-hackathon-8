d7cbb4e8c1b30ead56532c466ee42e5083a2cf44
Kai Daniel <kaiingodaniel@gmail.com>
2024-03-08 13:44:16 +0000

    choose test/llm version from cli

--- src/get_answer.py
+++ src/get_answer.py
@@ -1,6 +1,15 @@
-# from query_storage import main
+from sys import argv
 
-# get_answer = main
+wrong_arguments = ValueError("usage: fositive-vibes-only {test,llm}")
 
-def get_answer(query: str) -> str:
-    return f"<placeholder for response to to '{query}'"
+if len(argv) < 2:
+    raise wrong_arguments
+
+if argv[1] == "test":
+    def get_answer(query: str) -> str:
+        return f"<placeholder for response to to '{query}'"
+elif argv[1] == "llm":
+    from query_storage import main
+    get_answer = main
+else:
+    raise wrong_arguments

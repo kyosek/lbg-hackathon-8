2da05b8c9a222031dc043e5ca7f26891ecf44931
Kyosuke Morita <kyosuke1029@icloud.com>
2024-03-08 12:44:44 +0000

    reformat

--- src/query_storage.py
+++ src/query_storage.py
@@ -5,7 +5,7 @@ from langchain_community.embeddings.sentence_transformer import (
 from langchain.vectorstores import Chroma
 from transformers import pipeline
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from langchain.prompts import PromptTemplate
 from langchain.llms import HuggingFacePipeline
 from ragatouille import RAGPretrainedModel
@@ -17,6 +17,9 @@ RERANKER = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
 model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)
 tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
+eval_model = AutoModelForCausalLM.from_pretrained(EVALUATE_MODEL_NAME)
+eval_tokenizer = AutoTokenizer.from_pretrained(EVALUATE_MODEL_NAME)
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
 READER_LLM = pipeline(
     model=model,
@@ -29,25 +32,37 @@ READER_LLM = pipeline(
     max_new_tokens=500,
 )
 
+EVAL_LLM = pipeline(
+    model=eval_model,
+    tokenizer=eval_tokenizer,
+    task="text-generation",
+    do_sample=True,
+    temperature=0.1,
+    repetition_penalty=1.1,
+    return_full_text=False,
+    max_new_tokens=500,
+)
+
 llm = HuggingFacePipeline(pipeline=READER_LLM)
+eval_llm = HuggingFacePipeline(pipeline=EVAL_LLM)
 
 prompt_in_chat_format = [
     {
         "role": "system",
         "content": """Using the information contained in the context,
-give a comprehensive answer to the question.
-Respond only to the question asked, response should be concise and relevant to the question.
-Provide the number of the source document when relevant.
-If the answer cannot be deduced from the context, do not give an answer.""",
+        give a comprehensive answer to the query.
+        Respond only to the question asked, response should be concise and relevant to the question.
+        Provide the number of the source document when relevant.
+        If the answer cannot be deduced from the context, do not give an answer.""",
     },
     {
         "role": "user",
         "content": """Context:
-{context}
----
-Now here is the question you need to answer.
-
-Question: {question}""",
+        {context}
+        ---
+        Now here is the question you need to answer.
+        
+        Question: {question}""",
     },
 ]
 RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(
@@ -55,12 +70,7 @@ RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(
 )
 
 
-if __name__ == "__main__":
-    embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    db = Chroma(
-        persist_directory="chroma_db_sentence_trm",
-        embedding_function=embedding_function,
-    )
+def retrieve_context(query):
     docs = db.similarity_search(query, k=10)
 
     retrieved_docs_text = [
@@ -71,9 +81,61 @@ if __name__ == "__main__":
     context += "".join(
         [f"Document {str(i)}:::\n" + doc for i, doc in enumerate(reranked_relevant_docs)]
     )
+    return context
 
+
+def generate_response(query, context):
     final_prompt = RAG_PROMPT_TEMPLATE.format(question=query, context=context)
 
     response = llm(final_prompt)
+    return response
 
+
+def evaluate_response(query, context, response):
+    evaluation_prompt = PromptTemplate(
+        template="""You are a grader assessing the quality of the response given a user query and the context.\n
+            Here is the context document: \n {context} \n
+            Here is the user query: \n {query} \n
+            Here is the generated response: \n {response} \n
+            If the response does not include the key words from the question, the response is not good.\n
+            If the response does not contain the reference of the context, the response is not good.\n
+            Give a binary score 'yes' or 'no' score to indicate whether the response is relevant to the question. \n
+            If the binary score is 'yes', provide the score as a text variable with a single key 'score'
+            and no preamble or explanation.\n
+            If the binary score is 'no', re-write the query in the way it helps to retrieve more revelent context.
+            """,
+        input_variables=["query", "question", "context"],
+    )
+
+    eval_response = eval_llm(evaluation_prompt)
+
+    return eval_llm
+
+
+def main(query: str):
+    i = 0
+    db = Chroma(
+        persist_directory="chroma_db_sentence_trm",
+        embedding_function=embedding_function,
+    )
+    context = retrieve_context(query)
+    response = generate_response(query, context)
+
+    eval_response = evaluate_response(query, context, response)
+    if eval_response == "yes":
+        return response
+    else:
+        query = response
+        i += 1
+        while i < 3 & eval_response != "yes":
+            context = retrieve_context(query)
+            response = generate_response(query, context)
+
+            eval_response = evaluate_response(query, context, response)
+
+        return response
+
+
+if __name__ == "__main__":
+    response = main(query)
     print(response)

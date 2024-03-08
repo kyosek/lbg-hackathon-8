91ba01626525dea6a097cca5de2f6e366b179d17
Kai Daniel <kaiingodaniel@gmail.com>
2024-03-08 14:38:11 +0000

    fiddle around and add logging

--- src/query_storage.py
+++ src/query_storage.py
@@ -4,22 +4,35 @@ from langchain_community.embeddings.sentence_transformer import (
 )
 from langchain.vectorstores import Chroma
 from transformers import pipeline
-import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from langchain.prompts import PromptTemplate
 from langchain.llms import HuggingFacePipeline
 from ragatouille import RAGPretrainedModel
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S'
+)
+logger = logging.getLogger()
 
 query = "What happened to Mr. B?"
 READER_MODEL_NAME = "google/gemma-2b"
 EVALUATE_MODEL_NAME = "google/gemma-2b"
+
+logger.info("loading pretrained RAG model.")
 RERANKER = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
+logger.info("loading Auto parts.")
 model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)
 tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
-eval_model = AutoModelForCausalLM.from_pretrained(EVALUATE_MODEL_NAME)
-eval_tokenizer = AutoTokenizer.from_pretrained(EVALUATE_MODEL_NAME)
+# eval_model = AutoModelForCausalLM.from_pretrained(EVALUATE_MODEL_NAME)
+# eval_tokenizer = AutoTokenizer.from_pretrained(EVALUATE_MODEL_NAME)
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+db = Chroma(
+    persist_directory="chroma_db_sentence_trm",
+    embedding_function=embedding_function,
+)
 
 READER_LLM = pipeline(
     model=model,
@@ -32,19 +45,21 @@ READER_LLM = pipeline(
     max_new_tokens=500,
 )
 
-EVAL_LLM = pipeline(
-    model=eval_model,
-    tokenizer=eval_tokenizer,
-    task="text-generation",
-    do_sample=True,
-    temperature=0.1,
-    repetition_penalty=1.1,
-    return_full_text=False,
-    max_new_tokens=500,
-)
+# EVAL_LLM = pipeline(
+#     model=eval_model,
+#     tokenizer=eval_tokenizer,
+#     task="text-generation",
+#     do_sample=True,
+#     temperature=0.1,
+#     repetition_penalty=1.1,
+#     return_full_text=False,
+#     max_new_tokens=500,
+# )
 
+logger.info("Creating 'llm' object")
 llm = HuggingFacePipeline(pipeline=READER_LLM)
-eval_llm = HuggingFacePipeline(pipeline=EVAL_LLM)
+logger.info("Creating 'eval_llm' object")
+# eval_llm = HuggingFacePipeline(pipeline=EVAL_LLM)
 
 prompt_in_chat_format = [
     {
@@ -70,15 +85,17 @@ RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(
 )
 
 
-def retrieve_context(db, query):
-    docs = db.similarity_search(query, k=10)
+def retrieve_context(query):
+    logger.info("similarity search")
+    docs = db.similarity_search(query, k=1)
+    logger.info("similarity search done.")
 
     retrieved_docs_text = [
         doc.page_content for doc in docs
     ]
     context = "\nExtracted documents:\n"
     context += "".join(
-        [f"Document {i}:::\n{doc}" for i, doc in enumerate(retrieved_docs_text)]
+        [f"Document {str(i)}:::\n" + doc for i, doc in enumerate(retrieved_docs_text)]
     )
     return context
 
@@ -90,49 +107,51 @@ def generate_response(query, context):
     return response
 
 
-def evaluate_response(query, context, response):
-    evaluation_prompt = PromptTemplate(
-        template="""You are a grader assessing the quality of the response given a user query and the context.\n
-            Here is the context document: \n {context} \n
-            Here is the user query: \n {query} \n
-            Here is the generated response: \n {response} \n
-            If the response does not include the key words from the question, the response is not good.\n
-            If the response does not contain the reference of the context, the response is not good.\n
-            Give a binary score 'yes' or 'no' score to indicate whether the response is relevant to the question. \n
-            If the binary score is 'yes', provide the score as a text variable with a single key 'score'
-            and no preamble or explanation.\n
-            If the binary score is 'no', re-write the query in the way it helps to retrieve more revelent context.
-            """,
-        input_variables=["query", "question", "context"],
-    )
+# def evaluate_response(query, context, response):
+#     evaluation_prompt = PromptTemplate(
+#         template="""You are a grader assessing the quality of the response given a user query and the context.\n
+#             Here is the context document: \n {context} \n
+#             Here is the user query: \n {query} \n
+#             Here is the generated response: \n {response} \n
+#             If the response does not include the key words from the question, the response is not good.\n
+#             If the response does not contain the reference of the context, the response is not good.\n
+#             Give a binary score 'yes' or 'no' score to indicate whether the response is relevant to the question. \n
+#             If the binary score is 'yes', provide the score as a text variable with a single key 'score'
+#             and no preamble or explanation.\n
+#             If the binary score is 'no', re-write the query in the way it helps to retrieve more revelent context.
+#             """,
+#         input_variables=["query", "question", "context"],
+#     )
 
-    eval_response = eval_llm(evaluation_prompt)
+#     eval_response = eval_llm(evaluation_prompt)
 
-    return eval_llm
+#     return eval_llm
 
 
 def main(query: str):
     i = 0
-    db = Chroma(
-        persist_directory="chroma_db_sentence_trm",
-        embedding_function=embedding_function,
-    )
-    context = retrieve_context(db, query)
+    logger.info("loading Chroma.")
+
+    logger.info("calling 'retrieve_context'")
+    context = retrieve_context(query)
+    logger.info("calling 'generate_response'")
     response = generate_response(query, context)
 
-    eval_response = evaluate_response(query, context, response)
-    if eval_response == "yes":
-        return response
-    else:
-        query = response
-        i += 1
-        while i < 3 & eval_response != "yes":
-            context = retrieve_context(db, query)
-            response = generate_response(query, context)
+    # logger.info("calling 'evaluate_response'")
+    # eval_response = evaluate_response(query, context, response)
+    # if eval_response == "yes":
+    #     return response
+    # else:
+    #     query = response
+    #     i += 1
+    #     while i < 3 & eval_response != "yes":
+    #         context = retrieve_context(db, query)
+    #         response = generate_response(query, context)
 
-            eval_response = evaluate_response(query, context, response)
+            # eval_response = evaluate_response(query, context, response)
 
-        return response
+        # return response
+    return response
 
 
 if __name__ == "__main__":

574434b2f0d49d0bca20a1eed5ca62f439d94d2e
Kyosuke Morita <kyosuke1029@icloud.com>
2024-03-08 10:34:51 +0000

    add naive rag part

--- src/query_storage.py
+++ src/query_storage.py
@@ -3,8 +3,53 @@ from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
 from langchain.vectorstores import Chroma
+from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from langchain.prompts import PromptTemplate
+from langchain.llms import HuggingFacePipeline
 
 query = "What happened to Mr. B?"
+READER_MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
+
+model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
+
+READER_LLM = pipeline(
+    model=model,
+    tokenizer=tokenizer,
+    task="text-generation",
+    do_sample=True,
+    temperature=0.2,
+    repetition_penalty=1.1,
+    return_full_text=False,
+    max_new_tokens=500,
+)
+
+llm = HuggingFacePipeline(pipeline=READER_LLM)
+
+prompt_in_chat_format = [
+    {
+        "role": "system",
+        "content": """Using the information contained in the context,
+give a comprehensive answer to the question.
+Respond only to the question asked, response should be concise and relevant to the question.
+Provide the number of the source document when relevant.
+If the answer cannot be deduced from the context, do not give an answer.""",
+    },
+    {
+        "role": "user",
+        "content": """Context:
+{context}
+---
+Now here is the question you need to answer.
+
+Question: {question}""",
+    },
+]
+RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(
+    prompt_in_chat_format, tokenize=False, add_generation_prompt=True
+)
 
 if __name__ == "__main__":
     embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
@@ -12,5 +57,13 @@ if __name__ == "__main__":
         persist_directory="chroma_db_sentence_trm",
         embedding_function=embedding_function,
     )
-    docs = db.similarity_search(query)
-    print(docs[0].page_content)
+    docs = db.similarity_search(query, k=10)
+
+    retrieved_docs_text = [doc.page_content for doc in docs]  # we only need the text of the documents
+    context = "\nExtracted documents:\n"
+    context += "".join([f"Document {str(i)}:::\n" + doc for i, doc in enumerate(retrieved_docs_text)])
+
+    final_prompt = RAG_PROMPT_TEMPLATE.format(question=query, context=context)
+
+    response = llm(final_prompt)
+    print(response)
